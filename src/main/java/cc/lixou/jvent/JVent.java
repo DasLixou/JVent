68a6c93e36d7ca6fc3eb048644c0d2474f02acc5
DasLixou <einlixou@gmail.com>
2022-03-04 19:21:52 +0100

    👢 Added Call Shortcut

--- src/main/java/cc/lixou/jvent/JVent.java
+++ src/main/java/cc/lixou/jvent/JVent.java
@@ -9,6 +9,10 @@ public abstract class JVent {
 
     private static final HashMap<Class<? extends JVent>, Handler> handlers = new HashMap<>();
 
+    public static <U extends JVent> U call(U event) {
+        return getHandler(event.getClass()).call(event);
+    }
+
     public static Handler getHandler(Class<? extends JVent> event) {
         if(!handlers.containsKey(event)) {
             handlers.put(event, new Handler());
